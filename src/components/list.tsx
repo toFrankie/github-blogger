839a869f7ce6c417fa17c733805d9698f4ce61d1
Frankie <1426203851@qq.com>
2025-04-06 23:52:41 +0800

    feat: update issue list panel

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,20 +1,50 @@
-import {Drawer, Empty, Input, Pagination, Select, Space} from 'antd'
+import {TriangleDownIcon, XCircleFillIcon} from '@primer/octicons-react'
+import {Button, SelectPanel, Stack, TextInput} from '@primer/react'
+import {type ActionListItemInput} from '@primer/react/deprecated'
+import {Empty, Pagination} from 'antd'
 import {debounce} from 'licia-es'
-import {useCallback} from 'react'
+import {useCallback, useMemo, useState} from 'react'
 
-export default function List({store, visible, totalLabels, labels, totalCount, currentPage}) {
-  const selectedOptions = labels.map(item => item.name)
+// TODO: labels
+export default function List({store, visible, totalLabels, totalCount, currentPage}) {
+  const [titleValue, setTitleValue] = useState('')
+  const [selected, setSelected] = useState<ActionListItemInput[]>([])
+  const [filter, setFilter] = useState('')
+  const [open, setOpen] = useState(false)
 
-  const handleSelectChange = (e: any = []) => {
-    store.resetCurrentPage()
-    store.setFilterLabels(totalLabels.filter(o => e.includes(o.name)))
-    store.getIssues()
+  const items = useMemo(() => {
+    return totalLabels.map(item => ({text: item.name}))
+  }, [totalLabels])
+
+  const filteredItems = items.filter(
+    item =>
+      selected.some(selectedItem => selectedItem.text === item.text) ||
+      item.text?.toLowerCase().includes(filter.trim().toLowerCase())
+  )
+
+  const selectedItemsSortedFirst = filteredItems.sort((a, b) => {
+    const aIsSelected = selected.some(selectedItem => selectedItem.text === a.text)
+    const bIsSelected = selected.some(selectedItem => selectedItem.text === b.text)
+    if (aIsSelected && !bIsSelected) return -1
+    if (!aIsSelected && bIsSelected) return 1
+    return 0
+  })
+
+  const handleSelectedChange = (items: ActionListItemInput[]) => {
+    setSelected(items)
+    // TODO:
+    const labels = [...new Set(items.map(item => item.text as string).filter(Boolean))]
+    searchByLabel(labels)
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleInputChange = useCallback(
-    debounce(e => {
-      const title = e.target.value.trim()
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value
+    setTitleValue(title)
+    searchByTitle(title.trim())
+  }
+
+  const searchByTitle = useCallback(
+    debounce((title: string) => {
       store.setFilterTitle(title)
       store.resetCurrentPage()
       store.getIssues()
@@ -22,74 +52,111 @@ export default function List({store, visible, totalLabels, labels, totalCount, c
     []
   )
 
+  const searchByLabel = useCallback(
+    debounce((labels: string[]) => {
+      store.resetCurrentPage()
+      store.setFilterLabels(totalLabels.filter(l => labels.includes(l.name)))
+      store.getIssues()
+    }, 500),
+    []
+  )
+
   return (
-    <Drawer
-      closable={false}
-      open={visible}
-      placement="left"
-      style={{height: '100vh'}}
-      title="Issue List"
-      onClose={() => store.setListVisible(false)}
+    <div
+      className="drawer-container"
+      style={{
+        left: visible ? 0 : '-100vw',
+      }}
     >
-      <div className="app-issue-list">
-        <div className="issue-filter">
-          <Space direction="vertical" style={{width: '100%'}}>
-            <Input allowClear placeholder="Filter by title" onChange={handleInputChange} />
-            <Select
-              allowClear
-              showSearch
-              mode="multiple"
-              placeholder="Filter by labels"
-              style={{width: '100%'}}
-              value={selectedOptions}
-              onBlur={() => store.getIssues()}
-              onChange={handleSelectChange}
-              onClear={() => {
-                store.setFilterLabels([])
-                store.getIssues()
-              }}
-            >
-              {totalLabels.map(item => (
-                <Select.Option key={item.id} value={item.name}>
-                  {item.name}
-                </Select.Option>
-              ))}
-            </Select>
-          </Space>
-        </div>
-        <div className="list">
-          {store.issues.length > 0 ? (
-            store.issues.map(item => (
-              <div
-                key={item.id}
-                className="app-issue-list-item"
-                onClick={() => {
-                  store.setListVisible(false)
-                  store.setCurrentIssue(item)
-                }}
-              >
-                <div className="title">{item.title}</div>
-                <div className="number">#{item.number}</div>
-              </div>
-            ))
-          ) : (
-            <Empty />
-          )}
-        </div>
-        <div className="issue-pagination">
-          {totalCount > 20 ? (
-            <Pagination
-              showQuickJumper
-              current={currentPage}
-              pageSize={20}
-              showSizeChanger={false}
-              size="small"
-              total={totalCount}
-              onChange={page => store.setCurrentPage(page)}
-            />
-          ) : null}
+      <div
+        className="drawer-mask"
+        onClick={e => {
+          e.stopPropagation()
+          store.setListVisible(false)
+        }}
+      />
+      <div
+        className="drawer-content"
+        style={{left: visible ? 0 : '-100vw'}}
+        onClick={e => e.stopPropagation()}
+      >
+        <div className="app-issue-list">
+          <div className="issue-filter">
+            <Stack>
+              <TextInput
+                className="title-filter"
+                placeholder="Filter by title"
+                onChange={handleTitleChange}
+                value={titleValue}
+                trailingAction={
+                  titleValue ? (
+                    <TextInput.Action
+                      icon={XCircleFillIcon}
+                      aria-label="Clear input"
+                      onClick={() => setTitleValue('')}
+                    />
+                  ) : (
+                    <></>
+                  )
+                }
+              />
+
+              <SelectPanel
+                renderAnchor={({children, ...anchorProps}) => (
+                  <Button
+                    style={{width: '100%'}}
+                    {...anchorProps}
+                    alignContent="start"
+                    trailingAction={TriangleDownIcon}
+                    aria-haspopup="dialog"
+                  >
+                    {children}
+                  </Button>
+                )}
+                placeholder="Filter by label"
+                open={open}
+                onOpenChange={setOpen}
+                items={selectedItemsSortedFirst}
+                selected={selected}
+                onSelectedChange={handleSelectedChange}
+                onFilterChange={setFilter}
+              />
+            </Stack>
+          </div>
+          <div className="list">
+            {store.issues.length > 0 ? (
+              store.issues.map(item => (
+                <div
+                  key={item.id}
+                  className="app-issue-list-item"
+                  onClick={() => {
+                    store.setListVisible(false)
+                    store.setCurrentIssue(item)
+                  }}
+                >
+                  <div className="title">{item.title}</div>
+                  <div className="number">#{item.number}</div>
+                </div>
+              ))
+            ) : (
+              <Empty />
+            )}
+          </div>
+          <div className="issue-pagination">
+            {totalCount > 20 ? (
+              <Pagination
+                showQuickJumper
+                current={currentPage}
+                pageSize={20}
+                showSizeChanger={false}
+                size="small"
+                total={totalCount}
+                onChange={page => store.setCurrentPage(page)}
+              />
+            ) : null}
+          </div>
         </div>
       </div>
-    </Drawer>
+    </div>
   )
 }
