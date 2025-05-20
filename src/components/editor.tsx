0e0a7e5594fcfeac7839c8ffa114e77d3a51438f
Frankie <1426203851@qq.com>
2025-05-20 14:17:46 +0800

    refactor: reorganize code structure

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -7,7 +7,7 @@ import math from '@bytemd/plugin-math'
 import mediumZoom from '@bytemd/plugin-medium-zoom'
 import mermaid from '@bytemd/plugin-mermaid'
 import {Editor as BytemdEditor} from '@bytemd/react'
-import {Label, Stack, TextInput} from '@primer/react'
+import {Label, Stack, Text, TextInput} from '@primer/react'
 import {SkeletonText} from '@primer/react/experimental'
 import {message} from 'antd'
 
@@ -51,44 +51,51 @@ export default function Editor({
   }
 
   return (
-    <>
-      <div className="app-title">
-        <TextInput
-          className="title-input"
-          placeholder="Title"
-          value={issue.title}
-          onChange={e => onTitleChange(e.target.value)}
-        />
-        {issue.number > -1 && (
-          <div className="number" onClick={copyLink}>
-            #{issue.number}
-          </div>
-        )}
-      </div>
-      <div className="app-labels">
-        {isLoadingLabels ? (
-          <SkeletonText className="app-labels-skeleton" />
-        ) : (
-          <Stack direction="horizontal" gap="condensed" wrap="wrap">
-            {allLabel?.map(label => {
-              const checked = issue.labels.some(l => l.id === label.id)
-              return (
-                <Label
-                  key={label.id}
-                  size="small"
-                  variant={checked ? 'accent' : 'secondary'}
-                  onClick={() => (!checked ? onAddLabel(label) : onRemoveLabel(label))}
-                  sx={{fontSize: '11px'}}
-                >
-                  {label.name}
-                </Label>
-              )
-            })}
-          </Stack>
-        )}
-      </div>
-
-      <div className="app-editor">
+    <Stack className="app" gap="condensed" padding="condensed">
+      <Stack.Item sx={{flexShrink: 0}}>
+        <Stack direction="horizontal" align="center" gap="condensed">
+          <Stack.Item grow>
+            <TextInput
+              block
+              placeholder="Title"
+              value={issue.title}
+              onChange={e => onTitleChange(e.target.value)}
+            />
+          </Stack.Item>
+          {issue.number > -1 && (
+            <Stack.Item sx={{flexShrink: 0}}>
+              <Text onClick={copyLink} sx={{color: 'fg.muted', fontSize: 2, cursor: 'pointer'}}>
+                #{issue.number}
+              </Text>
+            </Stack.Item>
+          )}
+        </Stack>
+      </Stack.Item>
+      <Stack.Item sx={{flexShrink: 0}}>
+        <>
+          {isLoadingLabels ? (
+            <SkeletonText className="app-labels-skeleton" />
+          ) : (
+            <Stack direction="horizontal" gap="condensed" wrap="wrap">
+              {allLabel?.map(label => {
+                const checked = issue.labels.some(l => l.id === label.id)
+                return (
+                  <Label
+                    key={label.id}
+                    size="small"
+                    variant={checked ? 'accent' : 'secondary'}
+                    onClick={() => (!checked ? onAddLabel(label) : onRemoveLabel(label))}
+                    sx={{fontSize: '11px'}}
+                  >
+                    {label.name}
+                  </Label>
+                )
+              })}
+            </Stack>
+          )}
+        </>
+      </Stack.Item>
+      <Stack.Item grow className="app-editor">
         <BytemdEditor
           placeholder="Leave your thought here..."
           plugins={plugins}
@@ -97,7 +104,7 @@ export default function Editor({
           value={issue.body}
           onChange={onBodyChange}
         />
-      </div>
-    </>
+      </Stack.Item>
+    </Stack>
   )
 }
